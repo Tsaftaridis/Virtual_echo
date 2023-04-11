c547c06ffaa4a0386b76a0e30079174788b4b210
Tsaftaridis <tsaftaridis1@gmail.com>
2023-04-11 16:23:06 +0200

    Case 1 improvement
    * code.js changes to stop users from affecting answers.
    * Improved html text so that the experience flows more as a story and not as an algorithm.
    * Added a scoring system that takes into account how many wrong and right choices the trainee made to reach the end.
    * Added functionality to remove options that have already been pressed in diagnosis.
    * Added function to print out the score numbers in the end.

--- src/code.js
+++ src/code.js
@@ -1,5 +1,8 @@
 window.setup = window.setup || {};
 
+/* Disable history controls to stop users from affecting the scores. */
+Config.history.controls = false;
+
 /* hello world */ 
 /* window.hello = function (name) {
 	return "Hello " + name + ", how are you?";
