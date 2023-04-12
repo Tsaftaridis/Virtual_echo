4e1c46c0ce9b83b857bb2e1af1664483d1c629f8
Tsaftaridis <tsaftaridis1@gmail.com>
2023-04-12 10:36:03 +0200

    Style and backend
    User visible changes:
    * Changed colors in menus and main content to make it more appealing.
    * Added intro screen and score report screen.
    * Added random numbers to scoring report to throw trainees off of the scoring system.
    * Changed default menu to be collapsed.
    
    Structural changes:
    * Added multifile structure to support future case development.

--- src/code.js
+++ src/code.js
@@ -1,7 +1,9 @@
 window.setup = window.setup || {};
 
 /* Disable history controls to stop users from affecting the scores. */
-Config.history.controls = false;
+Config.history.controls = false; 
+
+Config.ui.stowBarInitially = true;
 
 /* hello world */ 
 /* window.hello = function (name) {
