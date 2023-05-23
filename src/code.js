3a820f9d78722ef6527ae23b55c594987ccec086
Tsaftaridis <Tsaftaridis@users.noreply.github.com>
2023-05-23 20:32:33 +0000

    deploy: c634e2eae6dc04bef745c24373105a442d91a145

--- src/code.js
+++ src/code.js
@@ -1,15 +1,10 @@
 window.setup = window.setup || {};
 
 /* Disable history controls to stop users from affecting the scores. (Un)comment the following line to toggle. */
-/* Config.history.controls = false; */
+Config.history.controls = false; 
 
 /* Hide sidebar initially. */ 
 Config.ui.stowBarInitially = true;
-
-/* hello world */ 
-/* window.hello = function (name) {
-	return "Hello " + name + ", how are you?";
-}; */ 
 
 setup.toSeconds = function (from, to) {
 	/* Determine the number of milliseconds between the two times. */
@@ -20,4 +15,3 @@ setup.toSeconds = function (from, to) {
 
 	return seconds;
 };
-
